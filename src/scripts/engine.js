bacbed45ba620ad6f35ebb65cac208b75f63796c
ranierearaujo <ranierepsa@gmail.com>
2023-12-12 20:27:05 -0300

    feat: Adding card details section behaviour

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,7 +6,14 @@ const state = {
     playerDeck: document.getElementById('player-deck'),
     enemyDeck: document.getElementById('enemy-deck'),
     playerVersus: document.getElementById('versus-player-card'),
-    enemyVersus: document.getElementById('versus-enemy-card')
+    enemyVersus: document.getElementById('versus-enemy-card'),
+    cardDetail: {
+        cardTitle: document.getElementById('detail-card-title'),
+        cardSubTitle: document.getElementById('detail-card-subtitle'),
+        cardImage: document.getElementById('detail-card-image'),
+        cardPower: document.getElementById('detail-card-power'),
+        cardDescription: document.getElementById('detail-card-description')
+    }
 }
 
 async function getChampionsData() {
@@ -43,19 +50,37 @@ function selectCard(event) {
     const enemyCard = getRandomEnemyCard();
     state.enemyVersus.src = enemyCard.src;
 
-    // Deletar carto do inimigo
+    // Deletar carta do inimigo
     enemyCard.remove();
+}
+
+async function updateDetailSection(event) {
+    const card = event.target;
+    const champion = JSON.parse(card.dataset.champion);
+
+    state.cardDetail.cardTitle.innerHTML = champion.name;
+    state.cardDetail.cardSubTitle.innerHTML = champion.title;
+
+    console.log(champion.info.difficulty);
+    console.log(Number(champion.info.difficulty));
+
+    state.cardDetail.cardPower.innerHTML = Number(champion.info.difficulty) * 500;
+    state.cardDetail.cardDescription.innerHTML = champion.blurb;
+    state.cardDetail.cardImage.src = await getChampionImageByName(champion.id);
 }
 
 async function drawCards(amount, isPlayer) {
     for (i = 0; i < amount; i++) {
         const cardImage = document.createElement('img');
         const champion = getRandomChampion();
+        cardImage.dataset.champion = JSON.stringify(champion);
         cardImage.src = await getChampionImageByName(champion.id);
         cardImage.classList.add('deck-card');
+
         if (isPlayer) {
             cardImage.classList.add('player-deck-card');
             cardImage.addEventListener('click', selectCard);
+            cardImage.addEventListener('mouseover', updateDetailSection);
             state.playerDeck.appendChild(cardImage);
         } else {
             state.enemyDeck.appendChild(cardImage);
